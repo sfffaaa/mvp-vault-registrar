7689da732b39f73a42e3ec1e59b298a15077d184
sfffaaa <sfffaaa@gmail.com>
2026-04-22 05:07:34 +0200

    fix: scope jest testMatch to test/ only

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ export default {
   extensionsToTreatAsEsm: [".ts"],
   moduleNameMapper: { "^(\\.{1,2}/.*)\\.js$": "$1" },
   testEnvironment: "node",
+  testMatch: ["<rootDir>/test/**/*.test.ts"],
   transform: {
     "^.+\\.tsx?$": ["ts-jest", { useESM: true }]
   }
